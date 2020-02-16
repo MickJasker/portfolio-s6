8bf756c342fd8a9e6f207b4c8bfdff4c5696e314
Mick Jasker <mick@swurl.nl>
2020-02-16 19:13:16 +0100

    Create Hero Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <Hero
+      subtitle="Portfolio Mick Jasker"
+      class="hero"
+      :bg-image="require('@/assets/images/berlin.jpg')"
+      :call-to-actions="[
+        {
+          copy: 'Bekijk mijn portfolio',
+          to: '/portfolio'
+        }
+      ]"
+    >
+      Semester <span class="highlight">6</span><br>
+      <span class="highlight">Startup</span> Experience
+    </Hero>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import Hero from '@/components/orgranisms/Hero.vue';
 
-export default {
-  name: 'Home',
+@Component({
   components: {
+    Hero,
   },
-};
+})
+export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped>
+  .hero {
+  }
+</style>
