7e7913b211b8341f79514a4d967c504f231c4023
Mick Jasker <mick@swurl.nl>
2020-02-20 13:56:44 +0100

    Add basic styled footer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
   <div id="app">
     <router-view />
+    <Footer />
   </div>
 </template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import Footer from '@/components/orgranisms/Footer.vue';
+
+@Component({
+  components: {
+    Footer,
+  },
+})
+export default class App extends Vue {}
+</script>
 
 <style lang="scss" src="./assets/scss/base.scss" />
---
+++ src/components/orgranisms/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer class="footer">
+    <h1>Mick Jasker<sup /> ⁄ 2016 – {{ new Date().getFullYear() }}</h1>
+    <h2>Alle rechten voorbehouden</h2>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+  footer.footer {
+    --color-bg: hsl(var(--color-neutral-100));
+    --color-text: hsl(var(--color-neutral-800));
+
+    background: var(--color-bg);
+    color: var(--color-text);
+
+    padding: 5rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h1 {
+      font-size: 3rem;
+      font-family: var(--font-base);
+    }
+
+    h2 {
+      font-size: 2rem;
+      color: hsl(var(--color-neutral-600));
+    }
+  }
+</style>
